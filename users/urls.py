7e0203cb18e0d29ecba1d79ce50b927314369ce6
snakejj <snakejj@github.com>
2021-03-30 17:21:11 +0200

    Adding "dashboard" page

--- users/urls.py
+++ users/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path('connexion/', views.login, name='login'),
     path('admin/', views.dashboard, name='dashboard'),
     path('logout/', views.logout, name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('activate/<uidb64>/<token>', views.account_activation, name='activate'),
     path('admin/videos', views.videos_list, name='videos_list'),
     path('admin/comments', views.comments_list, name='comments_list'),
 ]
--- users/views.py
+++ users/views.py
@@ -45,60 +45,6 @@ def comments_list(request):
     return render(request, 'users/comments_list.html', {'title': "Liste des commentaires"})
 
 
-
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            first_name = form.cleaned_data.get('first_name')
-            last_name = form.cleaned_data.get('last_name')
-            password = form.cleaned_data.get('password1')
-            email = form.cleaned_data.get('email')
-
-            user = User.objects.create_user(username=username, email=email)
-            user.set_password(password)
-            user.first_name = first_name
-            user.last_name = last_name
-            user.is_active = False
-            user.save()
-
-            current_site = get_current_site(request)
-
-            email_subject = 'Activer votre compte'
-            email_message = render_to_string(
-                'registration/activate.html',
-                {
-                    'user': user,
-                    'domain': current_site.domain,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': generate_token.make_token(user)
-                }
-            )
-
-            email_sender = EmailMessage(
-                email_subject,
-                email_message,
-                settings.EMAIL_HOST_USER,
-                [email]
-            )
-
-            email_sender.send()
-
-            messages.warning(
-                request,
-                "Merci de cliquer sur le lien envoyé dans votre boite mail !",
-                fail_silently=True
-            )
-            return redirect('users:login')
-    else:
-        form = SignUpForm()
-    return render(request, 'registration/signup.html', {
-        'form': form,
-        'title': "Inscription",
-        'header_title': "Inscription", })
-
-
 def logout(request):
     log_out(request)
     messages.info(request, "On espere vous revoir bientot !", fail_silently=True)
@@ -136,23 +82,4 @@ def login(request):
     return render(request, 'registration/login.html', {"form": form, 'title': "Connexion"})
 
 
-def account_activation(request, uidb64, token):
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-
-    # TODO : check doesnotexist
-    except User.DoesNotExist:
-        user = None
-
-    if user is not None and generate_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        messages.success(
-            request,
-            "Votre compte à été validé avec succès",
-            fail_silently=True
-        )
-        return redirect('core:home')
-    return render(request, 'registration/activate_failed.html', status=401)
 
