00f43e45f2cae7940a3c070f7d35a0366d0e3618
snakejj <snakejj@github.com>
2021-03-31 18:42:25 +0100

    Switching to PostgreSQL

---
+++ bubble/settings/production.py
@@ -0,0 +1,14 @@
+from . import *
+
+DEBUG = os.getenv("DEBUG_PROD")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': '',
+    }
+}
