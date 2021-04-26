6b6b27eaf5be1b8aef95f613dec9394c43169462
snakejj <snakejj@github.com>
2021-04-26 07:13:03 +0200

    "videos" app : Adding the admin side with custom template for easier moderation (videos displayed in admin)

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Video
+
+
+class VideoAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'status', 'link', 'added_on')
+    list_filter = ('status',)
+
+
+admin.site.register(Video, VideoAdmin)
