641b70b02c029f4eb20feae8cade4fbf172c5405
snakejj <snakejj@github.com>
2021-04-27 02:29:51 +0200

    "videos" app: Adding the report in the Admin

--- videos/admin.py
+++ videos/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.utils.html import format_html
 
-from .models import Video
+from .models import Video, AbuseVideo
 
 
 class VideoAdmin(admin.ModelAdmin):
@@ -12,3 +12,12 @@ class VideoAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Video, VideoAdmin)
+
+
+class AbuseVideoAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'added_on', 'video', "reason", 'message',)
+    list_filter = ('reason',)
+
+
+admin.site.register(AbuseVideo, AbuseVideoAdmin)
