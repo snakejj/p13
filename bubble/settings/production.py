88c35a306798194e649cd02f710f54b36e796981
snakejj <snakejj@github.com>
2021-05-06 18:06:50 +0200

    Adding Sentry SDK

--- bubble/settings/production.py
+++ bubble/settings/production.py
@@ -1,5 +1,23 @@
 from . import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 DEBUG = os.getenv("DEBUG_PROD")
 
 DATABASES = {
