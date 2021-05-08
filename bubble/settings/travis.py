205123e5a2876f45f7b1721d2030f0604069f294
snakejj <snakejj@github.com>
2021-05-08 18:51:31 +0200

    Adding travis files for continuous integration

---
+++ bubble/settings/travis.py
@@ -0,0 +1,14 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
+
+SECRET_KEY = "a%zfk(z%w3!#dcdqs7-vr%&p@-)ri%7ki!%w+_3)9eltcp0=@v"
