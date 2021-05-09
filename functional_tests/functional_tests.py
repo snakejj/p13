3f305e12bb1d27e97d0550283d98e6b6bbe8f73f
snakejj <snakejj@github.com>
2021-05-09 04:08:06 +0200

    Moving geckodriver and Fixing functional_tests

--- functional_tests/functional_tests.py
+++ functional_tests/functional_tests.py
@@ -22,18 +22,6 @@ class FunctionalTests(StaticLiveServerTestCase):
         )
         cls.driver.implicitly_wait(30)
         cls.driver.maximize_window()
-        mixer.blend('videos.Video', pk=3548, link="Qa7w5KeNsxc", status="IN", average_interest_rating=4.50,
-                    average_quality_rating=3.30)
-        mixer.blend('videos.Video', pk=6846, link="HqyJ7-ibUmo", status="IN", average_interest_rating=5.50,
-                    average_quality_rating=4.30)
-        mixer.blend('videos.Video', pk=8813, link="OgDZ3szraiY", status="IN", average_interest_rating=6.50,
-                    average_quality_rating=5.30)
-        mixer.blend('videos.Video', pk=9102, link="Yut87qR_thM", status="IN", average_interest_rating=7.50,
-                    average_quality_rating=2.30)
-        mixer.blend('videos.Video', pk=9358, link="jUdlNsgjJRY", status="IN", average_interest_rating=8.50,
-                    average_quality_rating=9.30)
-        mixer.blend('videos.Video', pk=9998, link="6JLgWX9iqgo", status="IN", average_interest_rating=9.50,
-                    average_quality_rating=7.30)
 
     @classmethod
     def tearDownClass(cls):
@@ -65,6 +53,19 @@ class FunctionalTests(StaticLiveServerTestCase):
         assert "Le lien est incorrect, merci de founir un lien Youtube valide" in self.driver.page_source
 
     def test_submit_video_which_already_exists_in_db(self):
+        mixer.blend('videos.Video', pk=3548, link="Qa7w5KeNsxc", status="IN", average_interest_rating=4.50,
+                    average_quality_rating=3.30)
+        mixer.blend('videos.Video', pk=6846, link="HqyJ7-ibUmo", status="IN", average_interest_rating=5.50,
+                    average_quality_rating=4.30)
+        mixer.blend('videos.Video', pk=8813, link="OgDZ3szraiY", status="IN", average_interest_rating=6.50,
+                    average_quality_rating=5.30)
+        mixer.blend('videos.Video', pk=9102, link="Yut87qR_thM", status="IN", average_interest_rating=7.50,
+                    average_quality_rating=2.30)
+        mixer.blend('videos.Video', pk=9358, link="jUdlNsgjJRY", status="IN", average_interest_rating=8.50,
+                    average_quality_rating=9.30)
+        mixer.blend('videos.Video', pk=9998, link="6JLgWX9iqgo", status="IN", average_interest_rating=9.50,
+                    average_quality_rating=7.30)
+
         self.driver.get(self.live_server_url)
         WebDriverWait(self.driver, 10)
 
@@ -89,6 +90,18 @@ class FunctionalTests(StaticLiveServerTestCase):
         assert "merci de proposer une autre" in self.driver.page_source
 
     def test_submit_valid_video_which_doesnt_exist_in_db(self):
+        mixer.blend('videos.Video', pk=3548, link="Qa7w5KeNsxc", status="IN", average_interest_rating=4.50,
+                    average_quality_rating=3.30)
+        mixer.blend('videos.Video', pk=6846, link="HqyJ7-ibUmo", status="IN", average_interest_rating=5.50,
+                    average_quality_rating=4.30)
+        mixer.blend('videos.Video', pk=8813, link="OgDZ3szraiY", status="IN", average_interest_rating=6.50,
+                    average_quality_rating=5.30)
+        mixer.blend('videos.Video', pk=9102, link="Yut87qR_thM", status="IN", average_interest_rating=7.50,
+                    average_quality_rating=2.30)
+        mixer.blend('videos.Video', pk=9358, link="jUdlNsgjJRY", status="IN", average_interest_rating=8.50,
+                    average_quality_rating=9.30)
+        mixer.blend('videos.Video', pk=9998, link="6JLgWX9iqgo", status="IN", average_interest_rating=9.50,
+                    average_quality_rating=7.30)
         self.driver.get(self.live_server_url)
         WebDriverWait(self.driver, 10)
 
